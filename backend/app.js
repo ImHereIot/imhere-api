7a1184df886d313ce77cfccf8dc97f4069171a5f
Julio Cesar <juliocesar@pop-os.localdomain>
2020-03-23 20:43:49 -0300

    adjust: server path

--- backend/app.js
+++ backend/app.js
@@ -3,12 +3,21 @@ const app = express();
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const studentsRoute = require('./packages/students/routes')
-const classRoute  = require('./packages/class/routes')
+const classRoute  = require('./packages/class/routes');
+const config = require('./config/config');
+const url  = config.bd_string;
 
-mongoose.connect("mongodb://localhost:3000", {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
+const options = {
+  reconnectTries: Number.MAX_VALUE,
+  reconnectInterval: 500,
+  poolSize: 5,
+  useNewUrlParser: true
+};
+
+mongoose.connect(url, options);
+mongoose.set("useCreateIndex", true);
+
+
 const aluno = mongoose.model("aluno", {
   name: String,
   aula: String,
