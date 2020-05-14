1b428b4fcbe88560e1f8ba74d2535ff05e85bd0f
Julio <juliolopes.bfg@gmail.com>
2020-05-14 00:34:40 -0300

    Adjust: api send

--- backend/packages/class/handlers.js
+++ backend/packages/class/handlers.js
@@ -2,15 +2,22 @@ const Class = require("./classModel/class");
 const handlers = {};
 
 handlers.get = async (req, res) => {
-  // if (!req.body.idAula) {
-  //   return res.status(400).send({
-  //     success: "false",
-  //     message: "O id da aula é necessário"
-  //   });
-  // }
-  // const classToFind = {
-  //   idAula : req.body.idAula
-  // }
+  if (!req.body.idAula) {
+    const classToFind = { idAula : req.body.idAula }
+    Class.find(classToFind, (err, docs) => {
+      if (err) {
+        return res.status(201).send({
+          success: "true",
+          err
+        });
+      }
+      return res.status(201).send({
+        success: "true",
+        docs
+      });
+    });
+  }
+  
   Class.find({}, (err, docs) => {
     if (err) {
       return res.status(201).send({
