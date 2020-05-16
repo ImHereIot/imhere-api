652a06c1acb852f64505e86168b8fd1577b5f26f
Julio <juliolopes.bfg@gmail.com>
2020-05-16 14:40:15 -0300

    aaa

--- backend/packages/class/classModel/class.js
+++ backend/packages/class/classModel/class.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const classModel = new mongoose.Schema({
   idAula: { type: String, required: true, unique: true, lowercase: true },
+  nomeAula: { type: String, required: true, lowercase: true },
   alunosCadastrados: { type: Array, required: true},
   professor: { type: String, required: true },
   idNFC: {type:String, required:true, unique:true },
