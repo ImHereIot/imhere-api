dd80a6771bbba98ee8536a816aa6baf03e4fd716
Julio <juliolopes.bfg@gmail.com>
2020-05-30 23:08:04 -0300

    adjusted path

--- backend/packages/studentsClass/routes.js
+++ backend/packages/studentsClass/routes.js
@@ -3,7 +3,6 @@ const router = express.Router();
 const handlers = require('./handlers');
 
 
-router.get('/', handlers.get);
 router.get('/:idAula', handlers.getStudents);
 router.put('/', handlers.put);
 router.post('/', handlers.post);
