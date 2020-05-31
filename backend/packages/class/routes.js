c53a105a143bf85cdfbec686a23469b52d9ea985
Julio <juliolopes.bfg@gmail.com>
2020-05-30 22:59:08 -0300

    changed routes

--- backend/packages/class/routes.js
+++ backend/packages/class/routes.js
@@ -5,7 +5,6 @@ const handlers = require('./handlers')
 
 //router.get('/:registro', handlers.get);
 router.get('/:idAula', handlers.getOne);
-router.get('/:idAula', handlers.getStudents);
 router.post('/', handlers.post);
 router.put('/:idAula', handlers.put);
 router.delete('/delete/:idAula', handlers.delete);
