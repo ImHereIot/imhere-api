5ee7a0861395c4048da2e9ba4c569ee9bf28e506
Julio <juliolopes.bfg@gmail.com>
2020-05-30 23:22:02 -0300

    changed path

--- backend/packages/class/routes.js
+++ backend/packages/class/routes.js
@@ -3,8 +3,8 @@ const router = express.Router();
 const handlers = require('./handlers')
 
 
-//router.get('/:registro', handlers.get);
-router.get('/:idAula', handlers.get);
+router.get('/:registro', handlers.get);
+//router.get('/:idAula', handlers.get);
 router.post('/', handlers.post);
 router.put('/:idAula', handlers.put);
 router.delete('/delete/:idAula', handlers.delete);
