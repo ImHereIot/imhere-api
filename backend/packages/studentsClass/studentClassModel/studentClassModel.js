c3bd9eeabba6d0cdffee75376ca7e7d5ecaae2ea
Julio <juliolopes.bfg@gmail.com>
2020-06-26 22:51:09 -0300

    s

--- backend/packages/studentsClass/studentClassModel/studentClassModel.js
+++ backend/packages/studentsClass/studentClassModel/studentClassModel.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const studentsClass = new mongoose.Schema({
   idAula: { type: String, required: true },
-  idPessoa: { type: Number, required: true },
+  idPessoa: { type: String, required: true },
   nomePessoa: { type: String, required: true },
   presenca: { type: Number, required: true },
   data: { type: Date },
